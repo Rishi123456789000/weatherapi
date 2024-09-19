528f8c3aa8e57140770f3109e99a955f9c12bfeb
Rishi123456789000 <rishivandari.18@gmail.com>
2024-09-19 15:32:50 +0530

    weather

---
+++ weather/src/api/weatherapi.jsx
@@ -0,0 +1,59 @@
+import axios from "axios";
+import { Component } from "react";
+
+export class Weather extends Component {
+  state = {
+    data: {}
+  }
+
+  componentDidMount() {
+    this.getLocation();
+  }
+
+  getLocation = () => {
+    navigator.geolocation.getCurrentPosition(
+      (position) => {
+        const { latitude, longitude } = position.coords;
+        if (latitude && longitude) {
+          this.fetch(latitude, longitude);
+        }
+      },
+      (error) => {
+        console.error("Error getting location:", error);
+      }
+    );
+  }
+
+  fetch = async (lat, lon) => {
+    try {
+      const newData = await axios.get(
+        `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=3d96b76bd18d71a187462ececa7bfe11`
+      );
+      this.setState({
+        data: newData.data
+      });
+    } catch (error) {
+      console.error("Error fetching weather data:", error);
+    }
+  }
+
+  render() {
+    const { data } = this.state;
+    const { name, main } = data || {}; // Use default empty object if data is null
+  
+    return (
+      <>
+        {name && main ? (
+          <>
+            <h1>Name: {name}</h1>
+            <h1>Humidity: {main.humidity}</h1>
+            <h1>Pressure: {main.pressure}</h1>
+            <h1>Temperature: {main.temp}</h1>
+          </>
+        ) : (
+          <h1>Loading...</h1>
+        )}
+      </>
+    );
+  }
+}
